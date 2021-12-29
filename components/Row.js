c42049f6cd2b3071356c3df7283cc8e9d2828ef0
p3lo <mysiacik@mysiacik.com>
2021-12-29 19:49:05 +0100

    .

--- components/Row.js
+++ components/Row.js
@@ -5,14 +5,14 @@ function Row({ task, index }) {
     <Draggable draggableId={index.toString()} index={index}>
       {(provided) => (
         <div className="w-full mb-2 bg-white">
-          <div
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            ref={provided.innerRef}
-            className="p-3 border"
-          >
-            <p className="flex-grow p-3 bg-white border">{task}</p>
-            {/* <button className="p-3 bg-slate-200">{task} sdfgdsf hsdfhsfdh s</button> */}
+          <div {...provided.draggableProps} ref={provided.innerRef} className="p-3 border">
+            <div className="flex">
+              <p className="flex-grow p-3 bg-white border">{task}</p>
+              {/* <button className="p-3 bg-slate-200">{task} sdfgdsf hsdfhsfdh s</button> */}
+              <p {...provided.dragHandleProps} className="p-2 border">
+                DragMe
+              </p>
+            </div>
           </div>
         </div>
       )}
